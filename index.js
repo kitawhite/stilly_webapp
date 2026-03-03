0dd973431e941ba38e8e7dafeb8429ecfcb44ce9
kitawhite <logicmatelogic2@gmail.com>
2026-03-03 23:28:22 +0800

    03-03-2026-2328_sgt

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+
+const textarea = document.querySelector('textarea')
+const addbutton = document.getElementById('AddButton')
+const todocontainer = document.querySelector('.TodoContainer')
+
+let todolist = []
+
+function addTodo() {
+    const todo = textarea.value
+    if (!todo)
+    {
+        return
+    }
+    console.log('Add todo: ',todo)
+    
+    todolist.push(todo)
+}
+
+addbutton.addEventListener('click', addTodo)
