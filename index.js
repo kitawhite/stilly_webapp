7c63fd02c28a1a42793fb1243c8b64e08d3e205d
kitawhite <logicmatelogic2@gmail.com>
2026-03-04 21:27:25 +0800

    04-03-2026-2127sgt

--- index.js
+++ index.js
@@ -13,12 +13,62 @@ function addTodo() {
         return
     }
     console.log('Add todo: ',todo)
-    
     todolist.push(todo)
+    textarea.value = ''
+    updateUI()
+}
+
+function editTodo(index) {
+    // Sets the textbox's text to be the same as the todo that has been selected
+    textarea.value = todoList[index]
+    todoList = todoList.filter((element, elementIndex) => {
+        if (index === elementIndex) {
+            // returning false to the filter function causes filter to REMOVE the selected element
+            return false
+        }
+        return true
+    })
+    updateUI()  
+}
+
+function deleteTodo(index) {
+    textarea.value = todoList[index]
+    todoList = todoList.filter((element, elementIndex) => {
+        if (index === elementIndex) {
+            return false
+        }
+        return true
+    })
+    updateUI()  
 }
 
 function updateUI() {
-    return
+    
+    let newInnerHTML = ''
+    todolist.forEach((todoElement, todoIndex) => {
+        // Note that for multiline strings, backticks are used insteead of the single apostrophe
+        newInnerHTML += ` 
+                <div class = "Todo">
+                    <p>${todoElement}
+                    </p>
+                    <div class = "ButtonContainer">
+                        <button class = "ButtonIcon" onclick ="editTodo(${todoIndex})">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                        </button>
+                        <button class = "ButtonIcon" onclick ="editTodo(${todoIndex})">
+                            <i class="fa-solid fa-trash"></i>
+                        </button>
+                    </div>
+                </div>
+        `
+
+    })
+    
+    todocontainer.innerHTML = newInnerHTML
+
+
+
+
 }
 
 addbutton.addEventListener('click', addTodo)
